7e36c5a80b686be5ada6002aff63c4e0699a29b8
Pooja B <123743783+Pooja-Babu17@users.noreply.github.com>
2023-02-14 07:25:38 -0800

    Add files via upload

---
+++ classpython.py
@@ -0,0 +1,42 @@
+class multifunction():
+    
+    def subfields():
+        print("sub-fields in AI are:")
+        print("Machine Learnings")
+        print("Neural Networks")
+        print("Vision")
+        print("Robotics")
+        print("Speech Processing")   
+        print("Natural Language Processing")
+        
+    def elegible ():
+        gender=input("your gender:")
+        age=int(input("your age:"))
+        if (age>=18):
+            print("not elegible") 
+            
+    def percentage():
+        sub1=int(input("subject1="))
+        sub2=int(input("subject2="))
+        sub3=int(input("subject3="))
+        sub4=int(input("subject4="))
+        sub5=int(input("subject5="))
+        total=sub1+sub2+sub3+sub4+sub5
+        print("total:",total)
+        print("Percentage",total/500*100)
+        
+    def triangle():
+        Height=int(input("Height:"))
+        breadth=int(input("Breadth:"))
+        area=float((Height*breadth)/2)
+        print("Area:",area) 
+        h1=int(input("height1:"))
+        h2=int(input("height2:"))
+        b1=int(input("breadth:"))
+        perimeter=h1+h2+b1
+        print("Perimeter of triangle",perimeter)
+        
+        
+   
+    
+   
